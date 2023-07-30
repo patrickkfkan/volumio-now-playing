3b4a8e699b42134998c46e84ef7f0805d473f4e0
patrickkfkan <patrickkfkan@gmail.com>
2023-07-31 05:56:13 +0800

    Do not recursively chmod in install script

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+
+USER_DIR="/data/INTERNAL/NowPlayingPlugin"
+CONFIG_BAK_DIR="${USER_DIR}/Settings Backups"
+MY_BG_DIR="${USER_DIR}/My Backgrounds"
+
 echo "Installing geo-tz node dependency"
 cd /data/plugins/user_interface/now_playing
 npm install --save geo-tz@"^7.0.1"
 echo "Creating user directories (if not exist)"
-mkdir -p "/data/INTERNAL/NowPlayingPlugin/Settings Backups"
-mkdir -p "/data/INTERNAL/NowPlayingPlugin/My Backgrounds"
-chmod -R 777 "/data/INTERNAL/NowPlayingPlugin"
+mkdir -p "${CONFIG_BAK_DIR}"
+mkdir -p "${MY_BG_DIR}"
+chmod 777 "${USER_DIR}"
+chmod 777 "${CONFIG_BAK_DIR}"
+chmod 777 "${MY_BG_DIR}"
 echo "Now Playing plugin installed"
 echo "plugininstallend"
